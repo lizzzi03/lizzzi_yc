cd955dacc7e18aeef44f678fa5eb912c67522bb4
lizzzi03 <yl4910@columbia.edu>
2023-05-07 23:50:11 -0400

    1

--- projects/project2/script.js
+++ projects/project2/script.js
@@ -1,12 +1,211 @@
 const art = [
-    {
-        name: "Azure Cliff with Red Maples",
-        author: "Gong Xian (Chinese, 1619-1689)",
-        year: "1680s",
-        catalog:"Fan",
-        image:"https://user-images.githubusercontent.com/124107292/236712187-e3e02fdf-f32a-4e93-9eb5-bf543f9c4203.jpg"
-    }
+{
+  name: "Azure Cliff with Red Maples",
+  author: "Wu Bin (active ca. 1583-1626)",
+  year: "1603",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236712187-e3e02fdf-f32a-4e93-9eb5-bf543f9c4203.jpg"
+},
+{
+  name: "Censer with figures in landscape",
+  author: "China",
+  year: "1707",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729324-4d908fd5-51b3-41c3-b17b-269af0ff1e46.jpg"
+},
+{
+  name: "Figure standing in a grove of trees",
+  author: "Li Liufang (Chinese, 1575-1629)",
+  year: "1613",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236729327-ab6e60c3-a7ac-4a27-a8c7-83c68d01c9b7.jpg"
+},
+{
+  name: "Landscape with solitary figure",
+  author: "Shitao (Zhu Ruoji) (Chinese, 1642-1707)",
+  year: "1660s",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729329-8ecd0df8-1a40-4c9f-867a-99c3bc2e76b2.jpg"
+},
+{
+  name: "Wine pot with figures in a landscape",
+  author: "China",
+  year: "1800",
+  catalog:"Artware",
+  image:"https://user-images.githubusercontent.com/124107292/236729333-ebf778ed-a499-4221-8a9e-353936619402.jpg"
+},
+{
+  name: "Listening to the Waterfall from a Moored Boat, in the manner of Wen Zhengming",
+  author: "Zhu Angzhi (Chinese, 1764-after 1841)",
+  year: "1837",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729336-af862561-da2e-4c79-90b7-ab55333e6f17.jpg"
+},
+{
+  name: "Dish with poem “The Pavilion of the Prince of Teng” by Du Fu (712-770)",
+  author: "China",
+  year: "1650",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729340-2927cb24-e06d-4892-b630-fb4c4828c679.jpg"
+},
+{
+  name: "Mount Qingcheng",
+  author: "Lu Yanshao (Chinese, 1909-1993)",
+  year: "1942",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729345-7d22f8fb-ceee-4981-835e-330cafb4cba5.jpg"
+},
+{
+  name: "Vase with equestrian figure in landscape",
+  author: "China",
+  year: "1650",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729347-e17e7244-671b-4888-8653-7c8c0fe1eba1.jpg"
+},
+{
+  name: "Landscape with figure",
+  author: "Qian Shoutie (Chinese, 1896-1967)",
+  year: "1950",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729351-d20f9c5d-e8b8-47a9-b7a1-357f191d766c.jpg"
+},
+{
+  name: "Poem on a Riverside Pavilion",
+  author: "Wang Duo (Chinese, 1592-1652)",
+  year: "1641",
+  catalog:"Calligraphy",
+  image:"https://user-images.githubusercontent.com/124107292/236729354-03606bc0-56e0-4804-af97-bd3f28ccbda9.jpg"
+},
+{
+  name: "Invitation to Reclusion at Jingxi",
+  author: "Dong Qichang (Chinese, 1555-1636)",
+  year: "1611",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729355-ae81db10-8f98-484c-a1a4-1d35b002f236.jpg"
+},
+{
+  name: "Streams and Mountains in Autumn Mist",
+  author: "Bian Wenyu (Chinese, active ca. 1620-70)",
+  year: "1634",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729359-a4ec095b-d495-4f72-b19c-6c4c968cb23b.jpg"
+},
+{
+  name: "Copy of Dong Qichang's Wanluan Thatched Cottage",
+  author: "Qian Du (Chinese, 1763-1844)",
+  year: "1803",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729360-e8346637-f923-43ac-bed3-3c571421e924.jpg"
+},
+{
+  name: "Brush Holder",
+  author: "Gu Jue (active late 17th century)",
+  year: "1775",
+  catalog:"Artware",
+  image:"https://user-images.githubusercontent.com/124107292/236729274-98394219-a544-4a15-b714-e97a07d833af.jpg"
+},
+{
+  name: "Landscapes with poems",
+  author: "Gong Xian (Chinese, 1619-1689)",
+  year: "1688",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729283-1e33e54a-fb28-4d6d-a007-cdae00ff8250.jpg"
+},
+{
+  name: "Vase with landscape",
+  author: "China",
+  year: "1725",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729285-16189ef8-8236-487c-8582-0200b10b4b3e.jpg"
+},
+{
+  name: "Vase with landscape scenes",
+  author: "China",
+  year: "1675",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729291-fefc3ad6-6407-413e-9a14-fb50c6e294f1.jpg"
+},
+{
+  name: "Vase with landscape",
+  author: "China",
+  year: "1675",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729295-7159735f-2f5a-49b6-a8e8-d36fbf24b2ef.jpg"
+},
+{
+  name: "Poem on the Double Ninth Festival",
+  author: "Chen Chun (Chinese, 1483-1544)",
+  year: "1500",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236729299-6aabb3b1-b298-4435-a504-a548bfcf6f09.jpg"
+},
+{
+  name: "Landscape with fisherman",
+  author: "Qi Zhijia (Chinese, active ca. 1627-after 1682)",
+  year: "1643",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236729301-e803f23c-a5ec-44c5-8750-c8218180ab85.jpg"
+},
+{
+  name: "Riverscape with moored boats",
+  author: "Wu Shantao (Chinese, 1624-after 1710)",
+  year: "1675",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236729303-2947c520-15a3-4163-941a-b64f01436afb.jpg"
+},
+{
+  name: "Qu Yuan and the Fisherman",
+  author: "Shen Hao (Chinese, active 1630-50)",
+  year: "1645",
+  catalog:"Fan",
+  image:"https://user-images.githubusercontent.com/124107292/236729306-62c4acba-00e8-4943-8221-6ac7a6e9e111.jpg"
+},
+{
+  name: "Landscapes",
+  author: "Gong Xian (Chinese, 1619-1689)",
+  year: "1680",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729308-9de50ac6-b52f-48dd-be19-80e226626c20.jpg"
+},
+{
+  name: "Drunken fisherman by a reed bank",
+  author: "Tang Yin (Chinese, 1470-1524)",
+  year: "1525",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729313-1580f997-43a2-469b-8838-71e5c048ec56.jpg"
+},
+{
+  name: "Rustic retreat among fishermen",
+  author: "Zhou Wenjing (Chinese, active ca. 1430-after 1463)",
+  year: "1400",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729314-637803ca-cf4c-4666-aa7c-b58af2e4b333.jpg"
+},
+{
+  name: "Joys of the Fisherman",
+  author: "Wang Fu (Chinese, 1362-1416)",
+  year: "1410",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729317-6e4dec7f-d30b-44fc-9fde-66a9fe130ace.jpg"
+},
+{
+  name: "Fisherman viewing the moon",
+  author: "Ye Guang (Chinese, active late 16th–early 17th century)",
+  year: "1460",
+  catalog:"Wash Painting",
+  image:"https://user-images.githubusercontent.com/124107292/236729320-12f0c4c8-b380-4aa8-b791-22e47d044e8d.jpg"
+},
+{
+  name: "Brush holder with scene of Wang Qing crossing the river from The Water Margin (Shuihu zhuan)",
+  author: "China",
+  year: "1636",
+  catalog:"Porcelain",
+  image:"https://user-images.githubusercontent.com/124107292/236729321-98c739fa-0971-47e7-9920-eee936e13a42.jpg"
+}
 ]
+
+
+
     const ul = document.querySelector("ul");
 
 function renderArtToPage(art) {
@@ -36,10 +235,10 @@ function renderArtToPage(art) {
 
       // append created elements to page
       ul.appendChild(list_item);
+      list_item.appendChild(image);
       list_item.appendChild(title);
       list_item.appendChild(author);
       list_item.appendChild(year);
-      list_item.appendChild(image);
     }
   }
 
