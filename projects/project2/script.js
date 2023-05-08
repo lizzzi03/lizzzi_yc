d5d381d91594e612d58361cf10f8c8ea4ea7db1d
lizzzi03 <yl4910@columbia.edu>
2023-05-07 20:40:53 -0400

    1

--- projects/project2/script.js
+++ projects/project2/script.js
@@ -4,7 +4,7 @@ const chairs = [
         author: "Gong Xian (Chinese, 1619-1689)",
         year: "1680s",
         catalog:"fan",
-        image:"https://collectiohttps://user-images.githubusercontent.com/124107292/236710422-45a70cdf-5bc3-4d1e-acbc-e5bc798320d8.jpgnapi.metmuseum.org/api/collection/v1/iiif/35990/150528/main-image"
+        image:"https://user-images.githubusercontent.com/124107292/236710422-45a70cdf-5bc3-4d1e-acbc-e5bc798320d8.jpg"
     }
 ]
     const ul = document.querySelector("ul");
